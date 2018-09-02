88fc8088bc1845b67e720a057438b687075fdc40
albertkosasi <albertkosasi96@gmail.com>
2018-09-02 19:12:56 +0700

    Week2Mobile

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LibraryPage } from '../pages/library/library';
+import { TabsPage } from '../pages/tabs/tabs';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { QuotePage } from '../pages/quote/quote';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { AddQuotePage } from '../pages/add-quote/add-quote';
+import { SettingsPage } from '../pages/settings/settings';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    FavoritesPage,
+    AddQuotePage,
+    SettingsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    FavoritesPage,
+    AddQuotePage,
+    SettingsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/library/library.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, Icon } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+import quotes from '../../data/quotes';
+
+/**
+ * Generated class for the LibraryPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html',
+})
+
+export class LibraryPage implements OnInit{
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LibraryPage');
+  }
+
+  ngOnInit () {
+    this.quoteCollection = quotes;
+    console.log(this.quoteCollection);
+  }
+
+
+}
